477ade79bcc510c84f4200c6f4ec7dfbd35261b5
Christian Weyer <christian.weyer@thinktecture.com>
2020-09-24 21:03:53 +0200

    CSS isolation

--- Client/Program.cs
+++ Client/Program.cs
@@ -20,7 +20,7 @@ namespace ConfTool.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped<IConferencesServiceClient, ConferencesServiceClientGrpc>();
             builder.Services.AddScoped<CountriesServiceClient>();
