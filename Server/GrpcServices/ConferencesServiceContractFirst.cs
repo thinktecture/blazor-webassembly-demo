a68d60c98fcc4db58a201a0d67f2221af59affae
Christian Weyer <christian.weyer@thinktecture.com>
2021-01-29 16:19:13 +0100

    Version bump

--- Server/GrpcServices/ConferencesServiceContractFirst.cs
+++ Server/GrpcServices/ConferencesServiceContractFirst.cs
@@ -1,24 +1,20 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Conference;
 using ConfTool.Server.Model;
 using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace ConfTool.Server.GrpcServices
 {
     public class ConferencesServiceContractFirst : Conferences.ConferencesBase
     {
-        private readonly ILogger<GreeterService> _logger;
         private readonly ConferencesDbContext _conferencesDbContext;
         private readonly IMapper _mapper;
 
-        public ConferencesServiceContractFirst(ILogger<GreeterService> logger, ConferencesDbContext conferencesDbContext, IMapper mapper)
+        public ConferencesServiceContractFirst(ConferencesDbContext conferencesDbContext, IMapper mapper)
         {
-            _logger = logger;
             _conferencesDbContext = conferencesDbContext;
             _mapper = mapper;
         }
